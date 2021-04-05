07bc2700d5196d9e0165f044102146562d13d70f
SunShiNe <daroxs95@gmail.com>
2021-04-05 13:08:23 -0400

    Update CMakeLists.txt and main.cpp

--- main.cpp
+++ main.cpp
@@ -9,19 +9,7 @@ using namespace std;
 
 struct app_state
 {
-
 };
-
-std::string strtohex(std::string data)
-{
-    std::stringstream res;
-
-    for (size_t i = 0; i <  data.size(); ++i)
-    {
-        std::cout << std::hex << std::setw(2) << static_cast<unsigned>(data[i]) << std::endl;
-    }
-    return res.str();
-}
 
 static void app(app_state &app_state, ImVec4 &clear_color );
 
@@ -60,9 +48,6 @@ static void app(app_state &state, ImVec4 &clear_color )
 
             ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
 
-            //not working
-            ImGui::PushItemWidth(ImGui::GetFontSize() * -12);           // Use fixed width for labels (by passing a negative value), the rest goes to widgets. We choose a width proportional to our font size.
-
             ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
 
             ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
@@ -81,15 +66,18 @@ static void app(app_state &state, ImVec4 &clear_color )
         //hex-string converter
         {
             ImGui::Begin("Hex-string converter");
+
             static string ascii_string = "This is the string to convert to hex-string";
             static string hex_string = "This is the string to convert to hex-string";
+
+            ImGui::PushItemWidth(ImGui::GetFontSize() * -10);           // Use fixed width for labels (by passing a negative value), the rest goes to widgets. We choose a width proportional to our font size.
 
             if (ImGui::InputTextMultiline("ascii-string", &ascii_string, ImVec2(0,50)))
             {
                 hex_string = string2hexstring(ascii_string);
             }
 
-            if (ImGui::InputTextMultiline("hex-string", &hex_string, ImVec2(0,50)))
+            if (ImGui::InputTextMultiline("hex-string", &hex_string, ImVec2(0,50), ImGuiInputTextFlags_CharsHexadecimal))
             {
                 ascii_string = hexstring2asciistring(hex_string);
             }
@@ -100,36 +88,63 @@ static void app(app_state &state, ImVec4 &clear_color )
         //RC4 cipher
         {
             ImGui::Begin("RC4 cipher");
+
             static string plaintext = "This is the plaintext";
             static string password;
             static string ciphertext = "This will be the ciphertext";
-            //static string ciphertext = "This will be the ciphertext";
+            static string S0_array = "This is First State Array S[0]";
+
+            ImGui::PushItemWidth(ImGui::GetFontSize() * -10);           // Use fixed width for labels (by passing a negative value), the rest goes to widgets. We choose a width proportional to our font size.
+
+            uint8_t tmepS0array[256];
+            std::stringstream tempStream;
 
             if(ImGui::InputTextWithHint("","password", &password))
             {
                 if(password.length() > 0)
                 {
                     RC4 cipher(password);
+                    
+                    cipher.getStateArray(tmepS0array);//get the S[0] array
+                    
+                    //convert S[0] array to string
+                    tempStream.clear();
+                    for (size_t i = 0; i < 256; i++)
+                    {
+                        tempStream << tmepS0array[i];
+                    }
+                    S0_array = string2hexstring(tempStream.str());
+
                     ciphertext = plaintext;
                     cipher.inplaceCipher(ciphertext);
                     ciphertext = string2hexstring(ciphertext);
                 }
             }
 
-            ImGui::InputTextWithHint("","First State Array", &password);
+            ImGui::InputTextMultiline("First State Array", &S0_array, ImVec2(0,60),ImGuiInputTextFlags_ReadOnly);
 
             if (ImGui::InputTextMultiline("plaintext", &plaintext, ImVec2(0,60)))
             {
                 if(password.length() > 0)
                 {
                     RC4 cipher(password);
+                    cipher.getStateArray(tmepS0array);//get the S[0] array
+                    
+                    //convert S[0] array to string
+                    tempStream.clear();
+                    for (size_t i = 0; i < 256; i++)
+                    {
+                        tempStream << tmepS0array[i];
+                    }
+                    S0_array = string2hexstring(tempStream.str());
+
                     ciphertext = plaintext;
                     cipher.inplaceCipher(ciphertext);
                     ciphertext = string2hexstring(ciphertext);
                 }
             }
 
-            if (ImGui::InputTextMultiline("ciphertext", &ciphertext, ImVec2(0,60)))
+            if (ImGui::InputTextMultiline("ciphertext", &ciphertext, ImVec2(0,60),ImGuiInputTextFlags_CharsHexadecimal))
             {
                 if(password.length() > 0)
                 {
