fcb896cbad92127b278a577d261d45847970ee31
SunShiNe <daroxs95@gmail.com>
2021-09-07 17:44:40 -0400

    Reworked lua integration

---
+++ tests/tests.cmake
@@ -0,0 +1,26 @@
+#TESTS
+### Add some usable files to test
+set (TestRequiredExtras
+)
+set (TESTS_FOLDER ${CMAKE_BINARY_DIR}/tests)
+
+foreach (_file ${TestRequiredExtras})
+        configure_file ( ${_file} ${_file} COPYONLY)
+endforeach()
+
+#test
+add_executable(test_lua_sol2
+               tests/lua_sol2.cpp
+)
+add_dependencies( test_lua_sol2 _Lua)
+include_directories (${_Lua_DIR}/include)
+target_link_directories ( test_lua_sol2 PRIVATE ${_Lua_DIR}/lib)
+
+target_link_libraries (test_lua_sol2 PRIVATE lua )#for sdl_opengl3 by find package
+
+set_target_properties(test_lua_sol2 PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${TESTS_FOLDER} )
+#target_include_directories(test_lua_sol2
+#    PRIVATE
+#    src
+#)
+add_test(NAME test_lua_sol2 COMMAND test_lua_sol2 WORKING_DIRECTORY ${TESTS_FOLDER})
