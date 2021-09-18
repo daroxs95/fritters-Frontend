9d8aeb099853883cee7c2c91134952ff8f9f108d
SunShiNe <daroxs95@gmail.com>
2021-09-17 20:43:06 -0400

    Fixed compilation in windows by workaround in lua_binders

--- src/lua_binders.h
+++ src/lua_binders.h
@@ -8,15 +8,15 @@
 
 #include <sol/sol.hpp>
 
+bool mCollapsingHeader(const char* label, bool* p_visible, ImGuiTreeNodeFlags flags)
+{
+    return ImGui::CollapsingHeader(label,p_visible,flags);
+}
 
-// C++ requires you to type out the same function body three times to obtain SFINAE-friendliness and 
-// noexcept-correctness. That's unacceptable.
-#define RETURNS(...) noexcept(noexcept(__VA_ARGS__)) -> decltype(__VA_ARGS__){ return __VA_ARGS__; }
-
-// The name of overload sets can be legally used as part of a function call - we can use a macro to
-// create a lambda for us that "lifts" the overload set into a function object.
-#define LIFT(f) [](auto&&... xs) RETURNS(f(::std::forward<decltype(xs)>(xs)...))
-
+bool mCollapsingHeaderSimple(const char* label, ImGuiTreeNodeFlags flags)
+{
+    return ImGui::CollapsingHeader(label, flags);
+}
 
 int bindImGui2sol2( sol::state &lua )
 {
@@ -25,14 +25,8 @@ int bindImGui2sol2( sol::state &lua )
     imgui["Text"]                                                           = ImGui::Text;
     imgui["Begin"]                                                          = ImGui::Begin;
     imgui["End"]                                                            = ImGui::End;
-    
-    std::function<bool(const char*, bool*, ImGuiTreeNodeFlags)> a           = LIFT(ImGui::CollapsingHeader);
-    imgui["CollapsingHeader"]                                               = a;
-
-    std::function<bool(const char*, ImGuiTreeNodeFlags)> b                  = LIFT(ImGui::CollapsingHeader);
-    imgui["CollapsingHeaderSimple"]                                         = b;
-
-
+    imgui["CollapsingHeader"]                                               = mCollapsingHeader;
+    imgui["CollapsingHeaderSimple"]                                         = mCollapsingHeaderSimple;
 
     return 0;
 }
